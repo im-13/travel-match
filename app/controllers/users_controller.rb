fb098ebc1773837bbea8effb2b8ea0a7681067db
Ilona Michalowska <i.michalowska@gmail.com>
2015-07-03 22:26:45 -0400

    Add flash message for user destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -103,7 +103,8 @@ class UsersController < ApplicationController
   def destroy
     User.find(params[:id]).destroy
     respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully desleted.' }
+      flash[:success] = "User was successfully deleted."
+      format.html { redirect_to users_url }
       format.json { head :no_content }
     end
   end
