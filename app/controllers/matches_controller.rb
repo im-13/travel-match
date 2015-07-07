df0fbc4ddba385e24bf2763d72e58623a6da3173
yan.lin <yan@bmobilized.com>
2015-07-07 16:42:14 -0400

    render index match

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -6,7 +6,19 @@ class MatchesController < ApplicationController
   end
 
   def index
-    @matches = @match.paginate(:page => params[:page], :per_page => 10, order: :first_name)
+=begin    
+    @matches = @matches.to_a
+
+    result_string = ""
+    @matches.each do |user|
+     result_string += "#{user.first_name}" + " #{user.last_name}" + ": #{user.email} " + " : #{user.get_age} yrs old \n"
+    end
+
+    clen = result.class
+    clen = result.length
+    render plain: "result class : \n #{result_string}"
+=end
+    @match
   end
 
   def specific_search
@@ -15,36 +27,26 @@ class MatchesController < ApplicationController
 
   def create
 
-    respond_to do |format|
-      user = current_user #makes call to the session helper function which will return the session user
-      if !user.nil?
-        username = user.email
-        target_country = user.query_as(:n).match("n-[:lives_in]->(country:Country)").pluck(:country).first 
-        user_birth = user.date_of_birth
-        #once country is determined we can do many thing 
-        @match = User.query_as(:n).match("n-[:lives_in]->(country:Country)").where("country.name = '#{target_country.name}' AND n.email <> '#{username}'").pluck(:n)
-        
-        #@match = reduce_by_age(user, @match)
+    user = current_user #makes call to the session helper function which will return the session user
+    if !user.nil?
+      username = user.email
+      target_country = user.query_as(:n).match("n-[:lives_in]->(country:Country)").pluck(:country).first 
+      user_birth = user.date_of_birth
+      #once country is determined we can do many thing 
+      @match = User.query_as(:n).match("n-[:lives_in]->(country:Country)").where("country.name = '#{target_country.name}' AND n.email <> '#{username}'").pluck(:n)
+      
+      #@match = reduce_by_age(user, @match)
 
-        #result_string = ""
-        #result.each do |user|
-        #  result_string += "#{user.first_name}" + " #{user.last_name}" + ": #{user.email} " + " : #{user.get_age} yrs old \n"
-        #end
-
-        #clen = result.class
-        #clen = result.length
-        #render plain: "result class : \n #{result_string}"
-
-        if @match
-          #flash[:success] = "TravelMatch found."
-          format.html { redirect_to mymatches_url} 
-          format.json { head :no_content }
-        else
-          flash[:danger] = "TravelMatch not found."
-          #format.html { render :new }
-        end
-      end  
-    end
+      if @match
+        #flash[:success] = "TravelMatch found."
+        #format.html { redirect_to mymatches_url} 
+        #format.json { head :no_content }
+        render "index"
+      else
+        flash[:danger] = "TravelMatch not found."
+        #format.html { render :new }
+      end
+    end  
   end
 
 end
