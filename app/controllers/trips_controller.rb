fdee7098d88cebf485541b8349917f3c7bf0aceb
Ilona Michalowska <i.michalowska@gmail.com>
2015-08-05 23:07:14 -0400

    Modify Alltrips view; add link to users that created trips

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -4,7 +4,7 @@ class TripsController < ApplicationController
   # GET /trips
   # GET /trips.json
   def index
-    @trips = Trip.all
+    @trips = Trip.all.paginate(:page => params[:page], :per_page => 5, :order => { updated_at: :desc })
   end
 
   # GET /trips/1
